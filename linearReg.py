52759396bca155e0b0f873d9295d194f1bf39620
Marco Fidel Avila <markos.avila@gmail.com>
2025-03-07 21:06:17 -0500

    prediccion de regresion lineal

--- linearReg.py
+++ linearReg.py
@@ -1,3 +1,34 @@
 import pandas as pd 
+import seaborn as sb
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
-datos =pd.read_csv('datosTemp.csv')
+datos = pd.read_csv('datosTemp.csv')
+
+datos.info()
+print(datos.head())  # Imprime las primeras filas del DataFrame para verificar las columnas
+#datos.head()
+
+# Crear el scatterplot
+sb.scatterplot(x="celsius", y="fahrenheit", data=datos, hue="fahrenheit", palette="coolwarm")
+
+# Mostrar el gráfico
+plt.show()
+
+#caracteristicas de los datos (X) y etiquetas (Y)
+X = datos['celsius']
+Y = datos['fahrenheit']
+
+print(X)
+print(Y)
+
+#X.values.reshape(-1,1) # esta transformacion es necesaria para que el modelo de regresion lineal pueda trabajar con los datos
+#Y.values.reshape(-1,1)
+
+X_procesada = X.values.reshape(-1,1)
+Y_procesada = Y.values.reshape(-1,1)
+
+modelo = LinearRegression()
+modelo.fit(X_procesada,Y_procesada) #entrenamiento del modelo, aca le entrego los datos de entrenamiento
+#print(modelo.coef_) #pendiente de la recta
+
